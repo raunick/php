976ba9f78ea6db8b5083058f94bfb500e349125a
Raunick Vileforte <raunickbhdesign@gmail.com>
2019-01-19 17:25:07 -0200

    correção de bug

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
    //digite sua altura na varial "a"
    $a = 1.75;
    //digite seu peso na varial "p"
-   $p = 75;
+   $p = 69;
    //calculo do IMC
    $calculo = $p/($a*$a);
 
-//tratamento de saida de casas decimais   
-echo $IMC = number_format($calculo,1,',','');
 
-if ($IMC < "18.5") {
-    echo " ----> abaixo do peso";
+//tratamento de saida de casas decimais   
+echo $IMC = number_format($calculo,0,',','');
+
+
+//condições
+if ($IMC < "18") {
+    echo " ----> Abaixo do peso";
 } 
-elseif ($IMC < "25.5") {
-    echo " ----> peso ideal";
+elseif ($IMC < "25") {
+    echo " ----> Peso ideal";
 } 
 else {
-    echo " ----> acima do peso";
+    echo " ----> Acima do peso";
 }
-?>
