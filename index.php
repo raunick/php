ad5471fed916b4a0ef16e3744ba490ceddfb97b7
Raunick Vileforte <raunickbhdesign@gmail.com>
2019-01-19 13:36:40 -0200

    Calculadora de IMC
    Calcula seu IMC - Índice de Massa Corporal

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php 
-    echo "Olá,Mundo!"
-?> 
+   //digite sua altura na varial "a"
+   $a = 1.75;
+   //digite seu peso na varial "p"
+   $p = 75;
+   //calculo do IMC
+   $IMC = $p/($a*$a);
+
+echo $IMC;
+
+if ($IMC < "18.5") {
+    echo " ----> abaixo do peso";
+} 
+elseif ($IMC < "25.5") {
+    echo " ----> peso ideal";
+} 
+else {
+    echo " ----> acima do peso";
+}
+?>
